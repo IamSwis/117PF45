13d5a9be116f174c6d2757a7978827e92f508ece
IamSwis <dre12sims@gmail.com>
2024-06-24 23:01:53 -0400

    V4

--- content/views.py
+++ content/views.py
@@ -5,3 +5,6 @@ from .models import Project
 def projects_list(request):
     projects = Project.objects.all()
     return render(request, "content/projects_list.html", {"projects": projects})
+
+def project_new(request):
+    return render(request, "content/projects_new.html")
